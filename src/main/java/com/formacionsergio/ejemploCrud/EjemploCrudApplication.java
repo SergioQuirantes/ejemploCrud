37bca48c03be029994e47a0c90a60465d0734b1e
Sergio Quirantes Vallés <squirantes@atsistemas.com>
2019-01-18 14:34:56 +0100

    Primer commit

---
+++ src/main/java/com/formacionsergio/ejemploCrud/EjemploCrudApplication.java
@@ -0,0 +1,14 @@
+package com.formacionsergio.ejemploCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploCrudApplication.class, args);
+	}
+
+}
+
