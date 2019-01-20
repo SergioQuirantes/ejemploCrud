f5875a87107e2081d83f6bcb51e77795ea4721b5
SergioQuirantes <sergiolordi@hotmail.es>
2019-01-20 17:34:18 +0100

    clase de configuracion creada

--- src/main/java/com/formacionsergio/ejemploCrud/model/TODO.java
+++ src/main/java/com/formacionsergio/ejemploCrud/model/TODO.java
@@ -1,5 +1,18 @@
 package com.formacionsergio.ejemploCrud.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TODO")
 public class TODO {
 
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
 }
